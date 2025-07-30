79c07cae5449e0c3a17418e2baf8d7a81e036def
efdee <filip@duyck.org>
2025-07-30 12:51:06 +0200

    samples

--- src/file/export/exportFile.ts
+++ src/file/export/exportFile.ts
@@ -7,41 +7,65 @@ import drawPackCanvas from 'src/utils/drawPackCanvas'
 import toBmfInfo from './toBmfInfo'
 import { ConfigItem } from './type'
 
-export default function exportFile(
-  project: Project,
-  config: ConfigItem,
-  fontName: string,
-  fileName: string,
-): void {
-  const zip = new JSZip()
-  const { packCanvas, glyphList, name, layout, ui } = project
-  const bmfont = toBmfInfo(project, fontName)
-  let text = config.getString(bmfont)
-  const saveFileName = fileName || name
+export function exportUnzippedFile(project: Project, config: ConfigItem, fontName: string, fileName: string) {
+    const { packCanvas, glyphList, name, layout, ui } = project
+    console.log('project name', project.name)
+    const bmfont = toBmfInfo(project, fontName)
+    let text = config.getString(bmfont)
+    const saveFileName = fileName || name
 
-  if (name !== saveFileName) {
-    text = text.replace(`file="${name}.png"`, `file="${saveFileName}.png"`)
-  }
+    if (name !== saveFileName) {
+        text = text.replace(`file="${name}.png"`, `file="${saveFileName}.png"`)
+    }
 
-  zip.file(`${saveFileName}.${config.ext}`, text)
+    writeFile(`${saveFileName}.${config.ext}`, text)
 
-  let canvas = createCanvas(ui.width, ui.height)
-  drawPackCanvas(canvas, packCanvas, glyphList, layout.padding)
+    let canvas = createCanvas(ui.width, ui.height)
+    drawPackCanvas(canvas, packCanvas, glyphList, layout.padding)
 
-  if (typeof window !== 'undefined') {
-    canvas.toBlob((blob: any) => {
-      if (blob) zip.file(`${saveFileName}.png`, blob)
-      zip
-        .generateAsync({ type: 'blob' })
-        .then((content) => saveAs(content, `${saveFileName}.zip`))
-    })
-  } else {
-    //console.log('no toBlob on canvas!')
-    const buffer = canvas.toBuffer('image/png')
+    if (typeof window !== 'undefined') {
+        canvas.toBlob((blob: any) => {
+            if (blob) writeFile(`${saveFileName}.png`, blob)
+        })
+    } else {
+        //console.log('no toBlob on canvas!')
+        const buffer = canvas.toBuffer('image/png')
+        writeFile(`${saveFileName}_0.png`, buffer)
+    }
+}
+
+export default function exportFile(project: Project, config: ConfigItem, fontName: string, fileName: string): void {
+    const zip = new JSZip()
+    const { packCanvas, glyphList, name, layout, ui } = project
+    const bmfont = toBmfInfo(project, fontName)
+    let text = config.getString(bmfont)
+    const saveFileName = fileName || name
+
+    if (name !== saveFileName) {
+        text = text.replace(`file="${name}.png"`, `file="${saveFileName}.png"`)
+    }
+
+    zip.file(`${saveFileName}.${config.ext}`, text)
+
+    let canvas = createCanvas(ui.width, ui.height)
+    drawPackCanvas(canvas, packCanvas, glyphList, layout.padding)
+
+    if (typeof window !== 'undefined') {
+        canvas.toBlob((blob: any) => {
+            if (blob) zip.file(`${saveFileName}.png`, blob)
+            zip.generateAsync({ type: 'blob' }).then((content) => saveAs(content, `${saveFileName}.zip`))
+        })
+    } else {
+        //console.log('no toBlob on canvas!')
+        const buffer = canvas.toBuffer('image/png')
+        const fs = eval("require('fs')")
+        zip.file(`${saveFileName}_0.png`, buffer)
+            .generateAsync({ type: 'nodebuffer' })
+            .then((content) => fs.writeFileSync(`${saveFileName}.zip`, content))
+    }
+}
+
+function writeFile(filePath: string, data: any) {
     const fs = eval("require('fs')")
-    zip
-      .file(`${saveFileName}_0.png`, buffer)
-      .generateAsync({ type: 'nodebuffer' })
-      .then((content) => fs.writeFileSync(`${saveFileName}.zip`, content))
-  }
+    fs.writeFileSync(filePath, data)
 }
--- src/file/export/toBmfInfo.ts
+++ src/file/export/toBmfInfo.ts
@@ -1,136 +1,117 @@
 import { Project } from 'src/store'
 
-import {
-  BMFont,
-  BMFontChars,
-  BMFontCommon,
-  BMFontInfo,
-  BMFontKernings,
-  BMFontPage,
-} from './type'
+import { BMFont, BMFontChars, BMFontCommon, BMFontInfo, BMFontKernings, BMFontPage } from './type'
 
 // http://www.angelcode.com/products/bmfont/doc/file_format.html
 export default function toBmfInfo(project: Project, fontFamily = ''): BMFont {
-  const {
-    name,
-    style,
-    layout,
-    globalAdjustMetric,
-    glyphList,
-    ui: { width, height },
-  } = project
-  const { opentype, size } = style.font
-  let fontScale = 1
-  if (opentype) {
-    fontScale = (1 / opentype.unitsPerEm) * size
-  }
-  const info: BMFontInfo = {
-    face: fontFamily || style.font.mainFamily,
-    size: style.font.size,
-    bold: 0,
-    italic: 0,
-    charset: '',
-    unicode: 1,
-    stretchH: 100,
-    smooth: 1,
-    aa: 1,
-    padding: new Array(4).fill(layout.padding),
-    spacing: new Array(2).fill(layout.spacing),
-  }
-
-  const common: BMFontCommon = {
-    lineHeight: Math.round(style.font.size * style.font.lineHeight),
-    base: Math.round(style.font.alphabetic - style.font.top),
-    scaleW: width,
-    scaleH: height,
-    pages: 1,
-    packed: 0,
-  }
-
-  const pages: BMFontPage[] = [
-    {
-      id: 0,
-      file: `${name}_0.png`,
-    },
-  ]
-
-  const chars: BMFontChars = {
-    count: glyphList.length,
-    list: [],
-  }
-
-  const kernings: BMFontKernings = {
-    count: 0,
-    list: [],
-  }
-
-  glyphList.forEach((glyph) => {
-    const isUnEmpty = !!(glyph.width && glyph.height)
-    const info = {
-      letter: glyph.letter,
-      id: glyph.letter.charCodeAt(0),
-      x: glyph.x,
-      y: glyph.y,
-      width: isUnEmpty ? glyph.width + layout.padding * 2 : 0,
-      height: isUnEmpty ? glyph.height + layout.padding * 2 : 0,
-      xoffset:
-        globalAdjustMetric.xOffset +
-        glyph.adjustMetric.xOffset -
-        (isUnEmpty ? glyph.trimOffsetLeft : 0) -
-        (isUnEmpty ? layout.padding : 0),
-      yoffset:
-        globalAdjustMetric.yOffset +
-        glyph.adjustMetric.yOffset -
-        (isUnEmpty ? glyph.trimOffsetTop : 0) -
-        (isUnEmpty ? layout.padding : 0),
-      xadvance:
-        Math.ceil(glyph.fontWidth) +
-        globalAdjustMetric.xAdvance +
-        glyph.adjustMetric.xAdvance,
-      page: 0,
-      chnl: 15,
-    }
-
-    chars.list.push(info)
-
+    const {
+        name,
+        style,
+        layout,
+        globalAdjustMetric,
+        glyphList,
+        ui: { width, height },
+    } = project
+    const { opentype, size } = style.font
+    let fontScale = 1
     if (opentype) {
-      glyphList.forEach(({ letter }) => {
-        const amount = Math.round(
-          opentype.getKerningValue(
-            opentype.charToGlyphIndex(glyph.letter),
-            opentype.charToGlyphIndex(letter),
-          ) *
-            fontScale +
-            (glyph.kerning.get(letter) || 0),
-        )
-        if (amount) {
-          kernings.list.push({
-            first: glyph.letter.charCodeAt(0),
-            second: letter.charCodeAt(0),
-            amount,
-          })
-        }
-      })
-    } else {
-      glyph.kerning.forEach((amount, letter) => {
-        if (amount)
-          kernings.list.push({
-            first: glyph.letter.charCodeAt(0),
-            second: letter.charCodeAt(0),
-            amount,
-          })
-      })
+        fontScale = (1 / opentype.unitsPerEm) * size
     }
-  })
-  kernings.count = kernings.list.length
+    const info: BMFontInfo = {
+        face: fontFamily || style.font.mainFamily,
+        size: style.font.size,
+        bold: 0,
+        italic: 0,
+        charset: '',
+        unicode: 1,
+        stretchH: 100,
+        smooth: 1,
+        aa: 1,
+        padding: new Array(4).fill(layout.padding),
+        spacing: new Array(2).fill(layout.spacing),
+    }
 
-  chars.list.sort((a, b) => a.id - b.id)
+    let fontSize = style.font.size + Math.min(0, style.shadow.offsetY) + style.stroke.width * 2
+    fontSize += 6 // extra padding to account for shadow/stroke rounding (?)
+    const common: BMFontCommon = {
+        //lineHeight: Math.round(style.font.size * style.font.lineHeight),
+        lineHeight: Math.round(fontSize /*style.font.size*/ * style.font.lineHeight),
+        base: Math.round(style.font.alphabetic - style.font.top),
+        scaleW: width,
+        scaleH: height,
+        pages: 1,
+        packed: 0,
+    }
 
-  return {
-    info,
-    common,
-    pages,
-    chars,
-    kernings,
-  }
+    const pages: BMFontPage[] = [
+        {
+            id: 0,
+            file: `${name}_0.png`,
+        },
+    ]
+
+    const chars: BMFontChars = {
+        count: glyphList.length,
+        list: [],
+    }
+
+    const kernings: BMFontKernings = {
+        count: 0,
+        list: [],
+    }
+
+    glyphList.forEach((glyph) => {
+        const isUnEmpty = !!(glyph.width && glyph.height)
+        const info = {
+            letter: glyph.letter,
+            id: glyph.letter.charCodeAt(0),
+            x: glyph.x,
+            y: glyph.y,
+            width: isUnEmpty ? glyph.width + layout.padding * 2 : 0,
+            height: isUnEmpty ? glyph.height + layout.padding * 2 : 0,
+            xoffset: globalAdjustMetric.xOffset + glyph.adjustMetric.xOffset - (isUnEmpty ? glyph.trimOffsetLeft : 0) - (isUnEmpty ? layout.padding : 0),
+            yoffset: globalAdjustMetric.yOffset + glyph.adjustMetric.yOffset - (isUnEmpty ? glyph.trimOffsetTop : 0) - (isUnEmpty ? layout.padding : 0),
+            xadvance: Math.ceil(glyph.fontWidth) + globalAdjustMetric.xAdvance + glyph.adjustMetric.xAdvance,
+            page: 0,
+            chnl: 15,
+        }
+
+        chars.list.push(info)
+
+        if (opentype) {
+            glyphList.forEach(({ letter }) => {
+                const amount = Math.round(
+                    opentype.getKerningValue(opentype.charToGlyphIndex(glyph.letter), opentype.charToGlyphIndex(letter)) * fontScale +
+                        (glyph.kerning.get(letter) || 0),
+                )
+                if (amount) {
+                    kernings.list.push({
+                        first: glyph.letter.charCodeAt(0),
+                        second: letter.charCodeAt(0),
+                        amount,
+                    })
+                }
+            })
+        } else {
+            glyph.kerning.forEach((amount, letter) => {
+                if (amount)
+                    kernings.list.push({
+                        first: glyph.letter.charCodeAt(0),
+                        second: letter.charCodeAt(0),
+                        amount,
+                    })
+            })
+        }
+    })
+    kernings.count = kernings.list.length
+
+    chars.list.sort((a, b) => a.id - b.id)
+
+    return {
+        info,
+        common,
+        pages,
+        chars,
+        kernings,
+    }
 }
